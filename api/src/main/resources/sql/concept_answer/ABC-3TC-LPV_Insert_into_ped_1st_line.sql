65c18acc1131be8c3a36ffaec14c360d03cc81b1
teggsdgreat <teggsdgreat@yahoo.co.uk>
2020-09-15 15:19:51 +0100

    Added Pharmacy Order Form

---
+++ api/src/main/resources/sql/concept_answer/ABC-3TC-LPV_Insert_into_ped_1st_line.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO `openmrs`.`concept_answer` 
+	(`concept_id`, `answer_concept`, `creator`, `date_created`, `uuid`, `sort_weight`) 
+VALUES 
+	('164507', '162200', '1', NOW(), '7huveyx5-d050-4e99-4826-ccddc456b8rf', '15');
