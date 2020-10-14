4a97285b048c28b6558e2ce09e67db38d188103f
Abdulfatai Toyeeb <TAbdulfatai@gmail.com>
2020-10-14 09:26:06 +0100

    Update

--- api/src/main/java/org/openmrs/module/nmrsmetadata/PatientQueuingClinicianLocationUUIDUpdate.java
+++ api/src/main/java/org/openmrs/module/nmrsmetadata/PatientQueuingClinicianLocationUUIDUpdate.java
@@ -15,7 +15,8 @@ public class PatientQueuingClinicianLocationUUIDUpdate implements Initializer {
 			//get the patient queuing clinician uuid property value
 			String clinicUUID = "patientqueueing.clinicianLocationUUIDS";
 			GlobalProperty globalProp = Context.getAdministrationService().getGlobalPropertyObject(clinicUUID);
-			String propertyVal = globalProp.getPropertyValue();
+			//String propertyVal = globalProp.getPropertyValue();
+			String propertyVal = "b1a8b05e-3542-4037-bbd3-998ee9c40574,2131aff8-2e2a-480a-b7ab-4ac53250262b,58c57d25-8d39-41ab-8422-108a0c277d98";
 			//set all locations uuid
 			String locUUID = ",7f65d926-57d6-4402-ae10-a5b3bcbf7986,7fdfa2cb-bc95-405a-88c6-32b7673c0453,aff27d58-a15c-49a6-9beb-d30dcfc0c66e,8ae6ceea-1891-44f4-8f45-55ba7c12a2d0,be7e1e5c-9bb1-4a0d-9683-4662b31b18dc,22294cb2-739a-43d2-8689-82bc42ca09da,fb1fc5b5-d2e6-4d2b-a8df-28c059d86991,8114f65f-53ca-444d-8e46-abc67b614692";
 			//concatenate property value to the locUUiD
