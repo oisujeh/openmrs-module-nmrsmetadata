ed1f634d2d80df7235b41cac8c06410b8862ab55
DESKTOP-6V6BPAA\MGIC <wandechris91@gmail.com>
2020-11-14 01:58:54 +0100

    Improved reports loading

---
+++ api/src/main/java/org/openmrs/module/nmrsmetadata/ReportsInitializer.java
@@ -0,0 +1,96 @@
+package org.openmrs.module.nmrsmetadata;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.service.DataSetDefinitionService;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.definition.service.ReportDefinitionService;
+import org.openmrs.module.reporting.serializer.ReportingSerializer;
+import org.openmrs.ui.framework.resource.ResourceFactory;
+import org.openmrs.ui.framework.resource.ResourceProvider;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ReportsInitializer implements Initializer {
+	
+	protected static final String providerName = "nmrsmetadata";
+	
+	protected static final String reportsPath = "reports/";
+	
+	protected static final Log log = LogFactory.getLog(ReportsInitializer.class);
+	
+	@Override
+	public void started() {
+		ReportDefinitionService reportDefinitionService = Context.getService(ReportDefinitionService.class);
+		DataSetDefinitionService dataSetDefinitionService = Context.getService(DataSetDefinitionService.class);
+		final ResourceFactory resourceFactory = ResourceFactory.getInstance();
+		final ResourceProvider resourceProvider = resourceFactory.getResourceProviders().get(providerName);
+		final File reportsDir = resourceProvider.getResource(reportsPath);
+		final List<String> reportPaths = new ArrayList<String>();
+		// Scanning the forms resources folder
+		if (reportsDir != null && reportsDir.listFiles() != null) {
+			for (File file : reportsDir.listFiles())
+				reportPaths.add(reportsPath + file.getName());
+		}
+		try {
+			ReportingSerializer reportingSerializer = new ReportingSerializer();
+			if (reportPaths.size() > 0) {
+				for (String reportsPath : reportPaths) {
+					try {
+						String xml = resourceFactory.getResourceAsString(providerName, reportsPath);
+						Object object = reportingSerializer.deserialize(xml, Object.class);
+						if (object instanceof SqlDataSetDefinition) {
+							SqlDataSetDefinition sqlDataSetDefinition = (SqlDataSetDefinition) object;
+							if (dataSetDefinitionService.getDefinitionByUuid(sqlDataSetDefinition.getUuid()) == null) {
+								sqlDataSetDefinition.setCreator(null);
+								sqlDataSetDefinition.setId(null);
+								sqlDataSetDefinition.setDateChanged(null);
+								sqlDataSetDefinition.setDateCreated(new Date());
+								dataSetDefinitionService.saveDefinition(sqlDataSetDefinition);
+							} else {
+								SqlDataSetDefinition old = (SqlDataSetDefinition) dataSetDefinitionService
+								        .getDefinitionByUuid(sqlDataSetDefinition.getUuid());
+								old.setSqlQuery(sqlDataSetDefinition.getSqlQuery());
+								old.setParameters(sqlDataSetDefinition.getParameters());
+								old.setDateChanged(new Date());
+								dataSetDefinitionService.saveDefinition(old);
+							}
+						} else if (object instanceof ReportDefinition) {
+							ReportDefinition reportDefinition = (ReportDefinition) object;
+							if (reportDefinitionService.getDefinitionByUuid(reportDefinition.getUuid()) == null) {
+								reportDefinition.setCreator(null);
+								reportDefinition.setId(null);
+								reportDefinition.setDateChanged(null);
+								reportDefinition.setDateCreated(new Date());
+								reportDefinitionService.saveDefinition(reportDefinition);
+							} else {
+								ReportDefinition old = reportDefinitionService.getDefinitionByUuid(reportDefinition
+								        .getUuid());
+								old.setDataSetDefinitions(reportDefinition.getDataSetDefinitions());
+								old.setParameters(reportDefinition.getParameters());
+								old.setDateChanged(new Date());
+								reportDefinitionService.saveDefinition(old);
+							}
+						}
+					}
+					catch (Exception e) {
+						log.error("Could not generate Report from the following resource file: " + reportsPath, e);
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
+	
+	@Override
+	public void stopped() {
+		
+	}
+}
--- api/src/main/resources/report/flag/patientflags_tag.sql
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-
-REPLACE INTO `patientflags_tag` VALUES (1, 'sys developer', NULL, 1, '2019-09-27 09:14:59', NULL, NULL, 0, NULL, NULL, NULL, '3883d897-cac5-4e3e-803d-788cbd075b45');
-
