6348770cd935cb9381ddf4d4aba7ee9611355a76
Abdulfatai Toyeeb <TAbdulfatai@gmail.com>
2021-05-06 20:19:23 +0100

    Form update

---
+++ api/src/main/resources/forms/forms/form_update.sql
@@ -0,0 +1,4 @@
+SET FOREIGN_KEY_CHECKS=0;
+UPDATE `form` SET encounter_type='37' WHERE form_id=44;
+UPDATE `form` SET name='Intensive Patient Follow Up Form MedAdhrAssessment' WHERE form_id=44;
+SET FOREIGN_KEY_CHECKS=1;
