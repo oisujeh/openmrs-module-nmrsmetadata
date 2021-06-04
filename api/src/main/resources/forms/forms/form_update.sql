964092859b588091a735753fc43f29b4c3464beb
Abdulfatai Toyeeb <TAbdulfatai@gmail.com>
2021-06-05 00:52:01 +0100

    point pmtct register to right encounter type

--- api/src/main/resources/forms/forms/form_update.sql
+++ api/src/main/resources/forms/forms/form_update.sql
@@ -1,4 +1,5 @@
 SET FOREIGN_KEY_CHECKS=0;
 UPDATE `form` SET encounter_type='37' WHERE form_id=44;
 UPDATE `form` SET name='Intensive Patient Follow Up Form MedAdhrAssessment' WHERE form_id=44;
+UPDATE `form` SET encounter_type='35' WHERE uuid='1b64c8bc-b289-45f7-943c-122be5de7274';
 SET FOREIGN_KEY_CHECKS=1;
